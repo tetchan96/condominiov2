463b0e6d3ffb968b6776a53c570e276f2b3c4b9b
Thiago Alves <thiagoalves@itatiaia.com.br>
2018-06-06 18:26:47 +0000

    Inserção de testes em MODELS

--- test/models/receita_recebida_test.rb
+++ test/models/receita_recebida_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
-class ReceitaRecebidaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class TemperatureTest < Minitest::Test
+  def receitatest
+      receita = Despesas.new
+      receita.valor = 100
+      receita.DescricaoTipo = DescricaoTipo.first
+      receita.morador = "Nome Morador"
+      
+      assert receita.save
+      
+     
+  end
+
+
+
+  def receitatest1
+      receita = Despesas.new
+      receita.valor = "R$100"
+      receita.DescricaoTipo = DescricaoTipo.first
+      receita.morador = Morador.first
+      
+      assert_not receita.save
+      
+    
+  end
+
+
+  def receitatest2
+      receita = Despesas.new
+      receita.valor = R$100
+      receita.DescricaoTipo = ""
+      receita.morador = ""
+      
+      assert_not receita.save
+      
+    
+  end
+  
+  def receitatests
+      receita = Despesas.new
+      receita.valor = 100,00
+      receita.DescricaoTipo = DescricaoTipo.first
+      receita.morador = ""
+      assert_not receita.save
+      
+    
+  end
+  
 end
+
+
